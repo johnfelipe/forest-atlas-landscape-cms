3d9355878af981af0fb0ea1684137ac90c6b6964
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-02-01 18:33:46 +0000

    Sends the list of widget ids to the back end

--- app/assets/javascripts/routers/management/OpenContentStepRouter.js
+++ app/assets/javascripts/routers/management/OpenContentStepRouter.js
@@ -24,6 +24,19 @@
       $('.js-form').on('submit', function () {
         if (this.wysiwygView) {
           var content = this.wysiwygView.getSerializedContent();
+
+          // We update the hidden field with the list of widgets
+          var widgetIds = content.ops.map(function (op) {
+            if (typeof op.insert === 'object' && typeof op.insert.widget === 'object') {
+              return op.insert.widget.id;
+            }
+            return null;
+          }).filter(function (id) {
+            return id !== null;
+          });
+          document.querySelector('.js-widget-ids').value = _.uniq(widgetIds).join(',');
+
+          // We save the content of the wysiwyg
           document.querySelector('.js-json-content').value = JSON.stringify(content);
         }
       }.bind(this));
